14b1e99736dcfd0ee8c9002aed13063724d78b65
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2020-12-24 23:36:59 +0500

    Checking for Auto deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <h1>Checking for auto deployment</h1>
     </div>
   );
 }
