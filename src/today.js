27300f360fcea56f7b7c5526c646b9f9f9277dc1
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2020-12-27 11:14:56 +0500

    Creating a Component

---
+++ src/today.js
@@ -0,0 +1,12 @@
+import React form 'react'
+
+function Today(){
+    return(
+        <div>
+            <h1>Today is Sunday</h1>
+            <h1>the time is 11:41am</h1>
+        </div>
+    )
+}
+
+export default Today;
