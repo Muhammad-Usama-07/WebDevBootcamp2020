664e222fcfc9aed233dee3c3eb3541c6624bdb7b
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2020-12-29 12:11:55 +0500

    Changing Day night condition

--- Class04/src/App.js
+++ Class04/src/App.js
@@ -6,11 +6,13 @@ function App() {
   let [count, setCount] = useState(1)
   let [isMorning, setMorning] = useState(true)
   return (
-    <div className = "box">
+    <div className = {`box ${isMorning ? 'daylight' : ''}`}>
       <h1>Day time = {isMorning ? 'Morning' : 'Night'}</h1>
       <Message counter = {count}/>
       <br />
       <button onClick={()=>setCount(count + 1)}>Update Counter</button>
+      <button onClick={()=>setMorning(!isMorning)}>Update Day</button>
+
     </div>
   );
 }
