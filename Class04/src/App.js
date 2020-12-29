8f4c3ffedc1eb4a53de5496227b56c69d6b752ad
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2020-12-29 11:23:00 +0500

    Changing value of variable

--- Class04/src/App.js
+++ Class04/src/App.js
@@ -2,7 +2,7 @@ import React , {useState} from 'react';
 import './App.css';
 
 function App() {
-  let [count, setCount] = useState(0)
+  let [count, setCount] = useState(1)
   return (
     <div >
       <h1>Value of the counter variable is: {count}</h1>
