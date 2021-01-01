8c41ca612618d87d8ee76092b56416923c90b9cd
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-01-01 20:34:53 +0500

    using State
    using State to print values from grand parent to grand child

--- Class05/src/App.js
+++ Class05/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React, {useState} from 'react' 
 import './App.css';
 import Parent from './Parent'
 import CounterContext from './CounterContext.js' 
 
 function App() {
+    let [count, SetCount] = useState(30)
   return (
-    <CounterContext.Provider value = {25} >
+    <CounterContext.Provider value = {count} >
         <div>
           <Parent name='Usama'/>
         </div>
