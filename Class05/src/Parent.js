983524be1dc29495b9ebb8adde02b2d0c8867477
Muhammad-Usama-07 <51862131+Muhammad-Usama-07@users.noreply.github.com>
2021-01-01 21:10:27 +0500

    accessing child component & removing props

--- Class05/src/Parent.js
+++ Class05/src/Parent.js
@@ -3,9 +3,11 @@ import React from 'react'
 import Child from './Child.js'
 import Child2 from './Child2.js'
 
-const Parent = (props) => {
+const Parent = () => {
     return(
-        <Child name={props.name}/>
+        <Child />
+        <Child2 />
+
     )
 }
         
