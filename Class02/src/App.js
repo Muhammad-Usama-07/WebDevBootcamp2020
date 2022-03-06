91ca16bf7d7afa9f99ec356e9b31bc4170cb76a2
Muhammad Usama <muhammadusama2k17@gmail.com>
2022-03-06 20:06:07 +0500

    Adding class 02

---
+++ Class02/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Checking for auto deployment02</h1>
+    </div>
+  );
+}
+
+export default App;
